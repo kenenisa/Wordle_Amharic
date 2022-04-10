d56bd814b5dc27e988715d79604a9632fc04a913
kenenisa alemayehu <kenenisaalemayhu0@gmail.com>
2022-04-10 05:39:49 +0300

    improved words data, added a bigger word set used to check if a word exists

--- server/Evaluate.js
+++ server/Evaluate.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const alphabet = JSON.parse(fs.readFileSync('./output.json').toString())
 const data = JSON.parse(fs.readFileSync('./data.json').toString())
+const allWords = JSON.parse(fs.readFileSync('./data.json').toString())
 const wordList = data.wordList;
 module.exports = (tried) => {
     const today = JSON.parse(fs.readFileSync('./todaysWord.json').toString()).Apr8
@@ -13,7 +14,7 @@ module.exports = (tried) => {
         fs.writeFileSync('./todaysWord.json', JSON.stringify({ Apr8: X }))
     }
     let result = []
-    if (wordList.find(word => word.word === tried.join(''))) {
+    if (allWords.wordList.find(word => word.word === tried.join(''))) {
         const spitPlace = (arr) => {
             const place = []
             arr.forEach(l => {
